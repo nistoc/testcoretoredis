c61eedb7052daf4acd5c5dbbba7f73255745067f
Nikita Temnikov <nistoc@gmail.com>
2019-03-29 19:33:49 +0300

    initial commit
    empty project was commited

---
+++ CoreToRedis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreToRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
